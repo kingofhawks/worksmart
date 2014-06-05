654f5bd8c54ce266417935a557e972089a853765
kingofhawks <kingofhawks@qq.com>
2014-06-05 16:39:03 +0800

    use url tag on template for URL name

--- bug/urls.py
+++ bug/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'worksmart.views.home', name='home'),
     url(r'^$', views.trend, name='trend'),
-    url(r'^data/$', views.trend_data, name='data'),
+    url(r'^trend_data/$', views.trend_data, name='trend_data'),
 )
