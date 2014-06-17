901762f3f386d201579080ae99acc0546c152c14
kingofhawks <kingofhawks@qq.com>
2014-06-17 11:26:15 +0800

    let models support i18n

--- bug/models.py
+++ bug/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class BugStatistics(models.Model):
-    total = models.SmallIntegerField()
-    closed = models.SmallIntegerField()
-    open = models.SmallIntegerField()
-    date = models.DateTimeField()
+    total = models.SmallIntegerField(verbose_name=_("total"))
+    closed = models.SmallIntegerField(verbose_name=_("closed"))
+    open = models.SmallIntegerField(verbose_name=_("open"))
+    date = models.DateTimeField(verbose_name=_("date"))
 
     class Meta:
+        verbose_name = _("BugStatistics")
         ordering = ['date']
 
     def __str__(self):
