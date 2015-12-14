627b80a802687b6e1eb8635f298d67fc210840ff
smartman <smartman2014@qq.com>
2015-12-14 16:32:12 +0800

    add test case for bill fields

--- node/test/bill.js
+++ node/test/bill.js
@@ -23,13 +23,31 @@ describe ( "BillList" , function ( ) {
     } ) ;
 } );
 
+
+describe ( "BillFields" , function ( ) {
+    it ( "should return fields of Bill" , function ( done ) {
+// calling bill fields API
+        server
+            . get ( "/healthmanager/rest/bill/fields" )
+            . expect ( "Content-type" ,/ json / )
+            . expect ( 200 )
+            . end ( function ( err , res ) {
+                console.log(res);
+                res. status.should . equal ( 200 ) ;
+                res. body.error.should.equal ( false ) ;
+                done ( ) ;
+            } ) ;
+    } ) ;
+} );
+
 describe ( "CreateBill" , function ( ) {
     it ( "should create Bill" , function ( done ) {
 // calling create bill API
         server
             .post( "/healthmanager/rest/bill/create" )
-            .set('Content-Type', 'application/x-www-form-urlencoded')
-            .send('{"FPicName":"test"}')
+            .set('Content-Type', 'multipart/form-data')
+            //.set('Content-Type', 'application/x-www-form-urlencoded')
+            .send({"FPicName":"test"})
             . expect ( "Content-type" ,/ json / )
             . expect ( 200 )
             . end ( function ( err , res ) {
