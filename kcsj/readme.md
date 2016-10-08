4980ade8d5a43a859a6427758e02e38ccf733539
smartman <smartman2014@qq.com>
2016-10-08 10:46:05 +0800

    勘察设计统计，直接读数据库

--- kcsj/readme.md
+++ kcsj/readme.md
@@ -1,5 +1,12 @@
 无锡市勘察设计数据统计模块
+@Deprecated
 1. 登陆勘察设计系统->合同备案->备案列表，选择"备案通过时间"，"已盖章"项目导出
 2. python analysis_xls.py
 如果是单项资质核验(导出Excel后要转成csv格式，否则read_excel报错)
 3. python analysis_xls_dx.py
+
+
+Please use this method for statistics:
+4. sync from production MS SQL Server(pip install pyodbc)    
+python sync_production.py
+python sync_production_dx.py
--- kcsj/sync_production_dx.py
+++ kcsj/sync_production_dx.py
@@ -16,25 +16,24 @@ def get_area(scale):
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.153.189;PORT=1433;DADABASE=Epoint_WXKCSJ;UID=sa;PWD=wxjsj^20150326')
 cursor = cnxn.cursor()
 
-cursor.execute("SELECT * FROM Epoint_WXKCSJ.dbo.HT_BA_Tab where BADate>='2016-09-01' and BADate<'2016-10-01'  order by BADate desc;")
+cursor.execute("select * from Epoint_WXKCSJ.dbo.ZZHY_DXSQB where SHDate>='2016-09-01' and SHDate<'2016-10-01'  order by SHDate desc")
 # SELECT * FROM Epoint_WXKCSJ.dbo.ZZHY_DXSQB order by operatedate desc
-row = cursor.fetchone()
-print row
-print row[1]
+# row = cursor.fetchone()
+# print row
+# print row[1]
 rows = cursor.fetchall()
 result = {}
 for row in rows:
-    print row[44], row[46], row[48], row[36], row[37], row[11]
-    industry = row[48]
+    print row[78], row[73], row[82], row[83], row[84]
+    industry = row[78]
     if result.get(industry):
         value = result.get(industry)
-        result.update({industry: {'count': value['count']+1, 'htje': row[36]+value['htje'],
-                                  'investment': row[37]+value['investment'],
-                                  'area': get_area(row[11])+value['area']}})
+        result.update({industry: {'count': value['count']+1, 'htje': row[82]+value['htje'],
+                                  'investment': row[83]+value['investment'],
+                                  'area': row[73]+value['area']}})
         # print industry, result.get(industry)
     else:
-        print 'area', get_area(row[11])
-        result.update({industry: {'count': 1, 'htje': row[36], 'investment': row[37], 'area': get_area(row[11])}})
+        result.update({industry: {'count': 1, 'htje': row[82], 'investment': row[83], 'area': row[73]}})
 
 for industry in result:
     print industry, result.get(industry)
